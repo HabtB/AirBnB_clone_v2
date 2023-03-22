afc7e08f40b562e627dd7f95287af3225f1e6620
HabtB <email.Habtamu@gmail.com>
2023-03-22 02:39:34 +0000

    setup mysql added

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,20 @@
+# Log into MySQL as the root user
+mysql -u root -p
+
+# Create the database hbnb_dev_db (if it doesn't already exist)
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+# Create the user hbnb_dev (if it doesn't already exist)
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+# Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+# Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+# Flush the privileges to ensure that the new permissions are loaded
+FLUSH PRIVILEGES;
+
+# Exit the MySQL prompt
+exit
