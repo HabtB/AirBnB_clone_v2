7e93d9a02c1b08e740dd21d114ef49beee4f4aa9
HabtB <email.Habtamu@gmail.com>
2023-03-22 03:07:31 +0000

    the setup sql was added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,17 @@
-# Create the database hbnb_dev_db (if it doesn't already exist)
+-- Create the database hbnb_dev_db (if it doesn't already exist)
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-# Create the user hbnb_dev (if it doesn't already exist)
+-- Create the user hbnb_dev (if it doesn't already exist)
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-# Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-# Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
-# Flush the privileges to ensure that the new permissions are loaded
+-- Flush the privileges to ensure that the new permissions are loaded
 FLUSH PRIVILEGES;
 
-# Exit the MySQL prompt
+-- Hbnb_dev_pwd1!Exit the MySQL prompt
 exit
