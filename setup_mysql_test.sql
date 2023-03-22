83c342f6490f006bc3a3f4efb35109cd495bff61
HabtB <email.Habtamu@gmail.com>
2023-03-22 17:26:35 +0000

    setup was updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,7 +9,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Flush the privileges to ensure that the new permissions are loaded
 FLUSH PRIVILEGES;
