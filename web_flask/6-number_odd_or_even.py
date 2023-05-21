df17ab14f8d02a9ecf4ae421c884ad1ca17c68e7
HabtB <email.Habtamu@gmail.com>
2023-05-21 16:06:33 +0000

    6-number_odd_or_even.py updated

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def even_odd(n=None):    
+def even_odd(n):
     """ returns for the '/number_odd_or_even/<int:n>' route"""
     if (type(n) == int):
         parity = "even" if (n % 2 == 0) else "odd"
